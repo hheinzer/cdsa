c3e521d215163e3b7f7b3d2fe5054ae25836c9d4
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2024-12-12 00:28:39 +0100

    add data_* functions as list methods; add documentation

--- list.c
+++ list.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// integer comparison function
 int intcmp(const void *a, const void *b)
 {
     const int *ia = a, *ib = b;
@@ -13,38 +14,59 @@ int intcmp(const void *a, const void *b)
 
 int main(void)
 {
-    List a = {0};
+    // create a list that stores integers
+    List a = list_create(sizeof(int), memcpy, intcmp, free);
 
-    for (long i = 0; i < 10; ++i) list_append(&a, &i, memcpy, sizeof(int));
+    // append integers 0 through 9 to the list
+    for (long i = 0; i < 10; ++i) list_append(&a, &i);
 
-    list_insert(&a, 3, (int[]){33}, memcpy, sizeof(int));
-    list_insert(&a, -3, (int[]){-33}, memcpy, sizeof(int));
+    // insert integer 33 at position 3
+    list_insert(&a, 3, (int[]){33});
 
-    List b = list_copy(&a, memcpy, sizeof(int));
+    // insert integer -33 at third-to-last position
+    list_insert(&a, -3, (int[]){-33});
 
+    // create a copy of the list
+    List b = list_copy(&a);
+
+    // remove the middle item from list a
     free(list_pop(&a, a.size / 2));
-    free(list_remove(&b, (int[]){33}, intcmp));
 
-    list_sort(&a, intcmp);
+    // remove the item with a value of 33 from list a
+    free(list_remove(&b, (int[]){33}));
+
+    // sort items of list a
+    list_sort(&a);
+
+    // sort items of list b with inverse compare function
+    list_sort_inverse(&b);
+
+    // reverse the order of list b
     list_reverse(&b);
 
+    // print items of list a
     printf("a: ");
     for (const ListItem *item = a.head; item; item = item->next)
         printf("%d%s", *(int *)item->data, (item->next ? ", " : "\n"));
 
+    // print items of list b
     printf("b: ");
     for (const ListItem *item = b.head; item; item = item->next)
         printf("%d%s", *(int *)item->data, (item->next ? ", " : "\n"));
 
-    printf("a.index(33) = %ld\n", list_index(&a, (int[]){33}, intcmp));
-    printf("b.index(33) = %ld\n", list_index(&b, (int[]){33}, intcmp));
+    // print index of item with a value of 33
+    printf("a.index(33) = %ld\n", list_index(&a, (int[]){33}));
+    printf("b.index(33) = %ld\n", list_index(&b, (int[]){33}));
 
-    printf("a.find(33) = %p\n", list_find(&a, (int[]){33}, intcmp));
-    printf("b.find(33) = %p\n", list_find(&b, (int[]){33}, intcmp));
+    // print address of item with a value of 33
+    printf("a.find(33) = %p\n", list_find(&a, (int[]){33}));
+    printf("b.find(33) = %p\n", list_find(&b, (int[]){33}));
 
-    printf("a.count(33) = %ld\n", list_count(&a, (int[]){33}, intcmp));
-    printf("b.count(33) = %ld\n", list_count(&b, (int[]){33}, intcmp));
+    // print number of occurrences of item with a value of 33
+    printf("a.count(33) = %ld\n", list_count(&a, (int[]){33}));
+    printf("b.count(33) = %ld\n", list_count(&b, (int[]){33}));
 
-    list_clear(&a, free);
-    list_clear(&b, free);
+    // clear items
+    list_clear(&a);
+    list_clear(&b);
 }
--- list.h
+++ list.h
@@ -3,56 +3,78 @@
 #include <assert.h>
 #include <stdlib.h>
 
+// general purpose doubly linked list
 typedef struct List List;
 typedef struct ListItem ListItem;
+typedef void *ListDataCopy(void *, const void *, size_t);
+typedef int ListDataCompare(const void *, const void *);
+typedef void ListDataFree(void *);
 
 struct List {
-    long size;
-    ListItem *head, *tail;
+    size_t data_size;           // size of item data in bytes
+    ListDataCopy *data_copy;    // pointer to a function for copying data
+    ListDataCompare *data_cmp;  // pointer to a function for comparing data
+    ListDataFree *data_free;    // pointer to a function for freeing data
+    long size;                  // number of items in list
+    ListItem *head, *tail;      // pointers to first and last item
 };
 
 struct ListItem {
-    void *data;
-    ListItem *next, *prev;
+    void *data;             // pointer to stored data
+    ListItem *next, *prev;  // pointers to next and previous item
 };
 
-static void list_insert(List *list, long i, void *data,
-                        void *data_copy(void *, const void *, size_t), size_t data_size)
+// create an empty list
+static List list_create(size_t data_size, ListDataCopy data_copy, ListDataCompare data_cmp,
+                        ListDataFree data_free)
+{
+    return (List){.data_size = data_size,
+                  .data_copy = data_copy,
+                  .data_cmp = data_cmp,
+                  .data_free = data_free};
+}
+
+// insert an item at a given position
+static void list_insert(List *list, long i, void *data)
 {
     assert(list);
     assert(-list->size <= i && i <= list->size);
+
+    // create item
     ListItem *item = calloc(1, sizeof(*item));
     assert(item);
-    if (data && data_copy) {
-        item->data = malloc(data_size);
+    if (data && list->data_copy) {
+        item->data = malloc(list->data_size);
         assert(item->data);
-        data_copy(item->data, data, data_size);
+        list->data_copy(item->data, data, list->data_size);
     }
     else {
         item->data = data;
     }
-    if (list->size == 0) {
+
+    // insert item
+    if (list->size == 0) {  // empty list
         list->head = item;
         list->tail = item;
     }
-    else if (i == 0 || i == -list->size) {
+    else if (i == 0 || i == -list->size) {  // item is first item
         item->next = list->head;
         list->head->prev = item;
         list->head = item;
     }
-    else if (i == list->size) {
+    else if (i == list->size) {  // insert is last item
         item->prev = list->tail;
         list->tail->next = item;
         list->tail = item;
     }
     else {
-        i = (list->size + i) % list->size;
+        i = (list->size + i) % list->size;  // convert possibly negative position
         ListItem *next = 0;
-        if (i <= list->size - 1 - i) {
+        if (i <= list->size - 1 - i) {  // search forward
             next = list->head;
             for (long n = 0; n < i; ++n) next = next->next;
         }
-        else {
+        else {  // search backward
             next = list->tail;
             for (long n = 0; n < list->size - 1 - i; ++n) next = next->prev;
         }
@@ -61,66 +83,78 @@ static void list_insert(List *list, long i, void *data,
         item->next->prev = item;
         item->prev->next = item;
     }
+
+    // housekeeping
     list->size += 1;
 }
 
-static void list_append(List *list, void *data, void *data_copy(void *, const void *, size_t),
-                        size_t data_size)
+// add an item to the end of the list
+static void list_append(List *list, void *data)
 {
-    list_insert(list, list->size, data, data_copy, data_size);
+    list_insert(list, list->size, data);
 }
 
+// remove the item at the given position in the list, and return its data
 static void *list_pop(List *list, long i)
 {
     assert(list);
-    if (list->size == 0) return 0;
+    if (list->size == 0) return 0;  // empty list
     assert(-list->size <= i && i < list->size);
+
+    // pop item
     ListItem *item = 0;
-    if (list->size == 1) {
+    if (list->size == 1) {  // item is first and only item
         item = list->head;
         list->head = 0;
         list->tail = 0;
     }
-    else if (i == 0 || i == -list->size) {
+    else if (i == 0 || i == -list->size) {  // item is first item
         item = list->head;
         list->head = item->next;
         list->head->prev = 0;
     }
-    else if (i == list->size - 1) {
+    else if (i == list->size - 1) {  // item is last item
         item = list->tail;
         list->tail = item->prev;
         list->tail->next = 0;
     }
     else {
-        i = (list->size + i) % list->size;
-        if (i <= list->size - 1 - i) {
+        i = (list->size + i) % list->size;  // convert possibly negative position
+        if (i <= list->size - 1 - i) {      // search forward
             item = list->head;
             for (long n = 0; n < i; ++n) item = item->next;
         }
-        else {
+        else {  // search backward
             item = list->tail;
             for (long n = 0; n < list->size - 1 - i; ++n) item = item->prev;
         }
         item->next->prev = item->prev;
         item->prev->next = item->next;
     }
+
+    // housekeeping
+    list->size -= 1;
+
+    // return item
     void *data = item->data;
     free(item);
-    list->size -= 1;
     return data;
 }
 
-static void *list_remove(List *list, const void *data, int data_cmp(const void *, const void *))
+// remove the first item from the list whose value is equal to data
+static void *list_remove(List *list, const void *data)
 {
     assert(list);
-    assert(data_cmp);
+    assert(list->data_cmp);
+
+    // remove item
     for (ListItem *item = list->head; item; item = item->next) {
-        if (!data_cmp(item->data, data)) {
-            if (item == list->head) {
+        if (!list->data_cmp(item->data, data)) {
+            if (item == list->head) {  // item is first item
                 list->head = item->next;
                 list->head->prev = 0;
             }
-            else if (item == list->tail) {
+            else if (item == list->tail) {  // item is last item
                 list->tail = item->prev;
                 list->tail->next = 0;
             }
@@ -128,54 +162,65 @@ static void *list_remove(List *list, const void *data, int data_cmp(const void *
                 item->next->prev = item->prev;
                 item->prev->next = item->next;
             }
+
+            // housekeeping
+            list->size -= 1;
+
+            // return item
             void *data = item->data;
             free(item);
-            list->size -= 1;
             return data;
         }
     }
+
+    // no matching item
     return 0;
 }
 
-static void list_clear(List *list, void data_free(void *))
+// remove all items from the list
+static void list_clear(List *list)
 {
     assert(list);
     for (ListItem *item = list->head, *next; item; item = next) {
         next = item->next;
-        if (data_free) data_free(item->data);
+        if (list->data_free) list->data_free(item->data);
         free(item);
     }
 }
 
-static long list_index(List *list, const void *data, int data_cmp(const void *, const void *))
+// return zero-based index in the list of the first item whose value is equal to data
+static long list_index(List *list, const void *data)
 {
     assert(list);
-    assert(data_cmp);
+    assert(list->data_cmp);
     long i = 0;
     for (const ListItem *item = list->head; item; item = item->next, ++i)
-        if (!data_cmp(item->data, data)) return i;
+        if (!list->data_cmp(item->data, data)) return i;
     return -1;
 }
 
-static void *list_find(List *list, const void *data, int data_cmp(const void *, const void *))
+// return the first item from the list whose value is equal to data
+static void *list_find(List *list, const void *data)
 {
     assert(list);
-    assert(data_cmp);
+    assert(list->data_cmp);
     for (const ListItem *item = list->head; item; item = item->next)
-        if (!data_cmp(item->data, data)) return item->data;
+        if (!list->data_cmp(item->data, data)) return item->data;
     return 0;
 }
 
-static long list_count(List *list, const void *data, int data_cmp(const void *, const void *))
+// return the number of times data appears in the list
+static long list_count(List *list, const void *data)
 {
     assert(list);
-    assert(data_cmp);
+    assert(list->data_cmp);
     long n = 0;
     for (const ListItem *item = list->head; item; item = item->next)
-        if (!data_cmp(item->data, data)) n += 1;
+        if (!list->data_cmp(item->data, data)) n += 1;
     return n;
 }
 
+// split a linked list into two halves
 static ListItem *x__list_merge_sort_split(ListItem *first)
 {
     ListItem *slow = first, *fast = first;
@@ -189,44 +234,60 @@ static ListItem *x__list_merge_sort_split(ListItem *first)
     return second;
 }
 
+// merge two sorted list into one sorted list
 static ListItem *x__list_merge_sort_merge(ListItem *first, ListItem *second,
-                                          int data_cmp(const void *, const void *))
+                                          int data_cmp(const void *, const void *), int order)
 {
     if (!first) return second;
     if (!second) return first;
-    if (data_cmp(first->data, second->data) < 0) {
-        first->next = x__list_merge_sort_merge(first->next, second, data_cmp);
+    if (order * data_cmp(first->data, second->data) < 0) {
+        first->next = x__list_merge_sort_merge(first->next, second, data_cmp, order);
         if (first->next) first->next->prev = first;
         first->prev = 0;
         return first;
     }
     else {
-        second->next = x__list_merge_sort_merge(first, second->next, data_cmp);
+        second->next = x__list_merge_sort_merge(first, second->next, data_cmp, order);
         if (second->next) second->next->prev = second;
         second->prev = 0;
         return second;
     }
 }
 
-static ListItem *x__list_merge_sort(ListItem *first, int data_cmp(const void *, const void *))
+// perform merge sort on list
+static ListItem *x__list_merge_sort(ListItem *first, int data_cmp(const void *, const void *),
+                                    int order)
 {
     if (!first || !first->next) return first;
     ListItem *second = x__list_merge_sort_split(first);
-    first = x__list_merge_sort(first, data_cmp);
-    second = x__list_merge_sort(second, data_cmp);
-    return x__list_merge_sort_merge(first, second, data_cmp);
+    first = x__list_merge_sort(first, data_cmp, order);
+    second = x__list_merge_sort(second, data_cmp, order);
+    return x__list_merge_sort_merge(first, second, data_cmp, order);
 }
 
-static void list_sort(List *list, int data_cmp(const void *, const void *))
+// sort the items of the list in place
+static void list_sort(List *list)
 {
     assert(list);
-    assert(data_cmp);
-    list->head = x__list_merge_sort(list->head, data_cmp);
+    assert(list->data_cmp);
+    list->head = x__list_merge_sort(list->head, list->data_cmp, 1);
     ListItem *item = list->head;
     while (item && item->next) item = item->next;
     list->tail = item;
 }
 
+// sort the items of the list in place, with inverse compare function
+static void list_sort_inverse(List *list)
+{
+    assert(list);
+    assert(list->data_cmp);
+    list->head = x__list_merge_sort(list->head, list->data_cmp, -1);
+    ListItem *item = list->head;
+    while (item && item->next) item = item->next;
+    list->tail = item;
+}
+
+// reverse the elements of the list in place
 static void list_reverse(List *list)
 {
     assert(list);
@@ -242,12 +303,11 @@ static void list_reverse(List *list)
     list->tail = swap;
 }
 
-static List list_copy(const List *list, void *data_copy(void *, const void *, size_t),
-                      size_t data_size)
+// return a copy of the list
+static List list_copy(const List *list)
 {
     assert(list);
-    List copy = {0};
-    for (const ListItem *item = list->head; item; item = item->next)
-        list_append(&copy, item->data, data_copy, data_size);
+    List copy = list_create(list->data_size, list->data_copy, list->data_cmp, list->data_free);
+    for (const ListItem *item = list->head; item; item = item->next) list_append(&copy, item->data);
     return copy;
 }

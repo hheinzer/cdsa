d922c38e776354dcad68bb4f832effa2a4d3105c
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2024-12-17 17:18:54 +0100

    test for minimum and maximum amount of resizing

--- test/dict_vs_hmap.c
+++ test/dict_vs_hmap.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 #include "../src/dict.h"
@@ -10,51 +9,74 @@ void stress_dict(long n, long capacity, float load_factor, DictKeyHash hash);
 void stress_hmap(long n, long capacity, float load_factor, HmapKeyHash hash);
 char *number(char *str, long num);
 
+/* Compiled with 'clang -std=c23 -march=native -Ofast -DNDEBUG' I get:
+ * - for m = 0 (maximum amount of resizing):
+ *   0.05:     1.5534,     1.3444,     0.1345
+ *   0.10:     2.0774,     1.7301,     0.1672
+ *   0.15:     1.1148,     0.9791,     0.1218
+ *   0.20:     0.9256,     0.8227,     0.1111
+ *   0.25:     1.1039,     0.9411,     0.1474
+ *   0.30:     0.9032,     0.7847,     0.1312
+ *   0.35:     0.9258,     0.7964,     0.1398
+ *   0.40:     0.9479,     0.8085,     0.1470
+ *   0.45:     0.9487,     0.7885,     0.1689
+ *   0.50:     0.9087,     0.7482,     0.1766
+ *   0.55:     0.9908,     0.7832,     0.2096
+ *   0.60:     1.0357,     0.7993,     0.2283
+ *   0.65:     1.0353,     0.8250,     0.2032
+ *   0.70:     1.2060,     0.8332,     0.3091
+ *   0.75:     1.1596,     0.8234,     0.2899
+ *   0.80:     1.3033,     0.9131,     0.2994
+ *   0.85:     1.4334,     1.0385,     0.2755
+ *   0.90:     1.7734,     1.4115,     0.2041
+ *   0.95:     2.5671,     2.7638,    -0.0766
+ * - for m = n (no resizing):
+ *   0.05:     1.2129,     1.0731,     0.1153
+ *   0.10:     0.9908,     0.8953,     0.0964
+ *   0.15:     0.9227,     0.8381,     0.0918
+ *   0.20:     0.8889,     0.8096,     0.0892
+ *   0.25:     0.8741,     0.7889,     0.0974
+ *   0.30:     0.8644,     0.7756,     0.1027
+ *   0.35:     0.8598,     0.7659,     0.1093
+ *   0.40:     0.8598,     0.7582,     0.1181
+ *   0.45:     0.8589,     0.7505,     0.1262
+ *   0.50:     0.8601,     0.7475,     0.1310
+ *   0.55:     0.8629,     0.7398,     0.1427
+ *   0.60:     0.8657,     0.7307,     0.1559
+ *   0.65:     0.8720,     0.7274,     0.1658
+ *   0.70:     0.8717,     0.7203,     0.1736
+ *   0.75:     0.8758,     0.7174,     0.1809
+ *   0.80:     0.8796,     0.7120,     0.1905
+ *   0.85:     0.8841,     0.7172,     0.1888
+ *   0.90:     0.8881,     0.7278,     0.1805
+ *   0.95:     0.8933,     0.7742,     0.1333
+ *
+ * This show that hmap is consistently faster than dict, except for very  high low load factors,
+ * where both data structures perform badly. I get similar results if compiled with gcc.
+ */
 int main(void)
 {
-    const long n = 1'000'000, capacity = n;
+    const long n = 2'000'000, m = n;
     for (float load_factor = 0.05; load_factor < 1.0; load_factor += 0.05) {
-        stress_dict(n, capacity, load_factor, strhash_fnv1a);
+        stress_dict(n, m / load_factor + 1, load_factor, strhash_fnv1a);
         const clock_t t0 = clock();
-        stress_dict(n, capacity, load_factor, strhash_fnv1a);
+        stress_dict(n, m / load_factor + 1, load_factor, strhash_fnv1a);
         const clock_t t1 = clock();
 
-        stress_hmap(n, capacity, load_factor, strhash_fnv1a);
+        stress_hmap(n, m / load_factor + 1, load_factor, strhash_fnv1a);
         const clock_t t2 = clock();
-        stress_hmap(n, capacity, load_factor, strhash_fnv1a);
+        stress_hmap(n, m / load_factor + 1, load_factor, strhash_fnv1a);
         const clock_t t3 = clock();
 
         printf("%.2f: %10.4f, %10.4f, %10.4f\n", load_factor, (t1 - t0) / (double)CLOCKS_PER_SEC,
                (t3 - t2) / (double)CLOCKS_PER_SEC, 1 - (t3 - t2) / (double)(t1 - t0));
     }
-    // Compiled with 'clang -std-c23 -march=native -Ofast -flto=auto -DNDEBUG' I get:
-    // 0.05:     0.5689,     0.5045,     0.1132
-    // 0.10:     0.4696,     0.4192,     0.1072
-    // 0.15:     0.4330,     0.3857,     0.1093
-    // 0.20:     0.4172,     0.3675,     0.1190
-    // 0.25:     0.4144,     0.3550,     0.1432
-    // 0.30:     0.4112,     0.3434,     0.1650
-    // 0.35:     0.4038,     0.3355,     0.1690
-    // 0.40:     0.4010,     0.3282,     0.1816
-    // 0.45:     0.4041,     0.3220,     0.2030
-    // 0.50:     0.3961,     0.3098,     0.2180
-    // 0.55:     0.4126,     0.3068,     0.2565
-    // 0.60:     0.4144,     0.3071,     0.2588
-    // 0.65:     0.4157,     0.3013,     0.2752
-    // 0.70:     0.4327,     0.3068,     0.2909
-    // 0.75:     0.4259,     0.2934,     0.3110
-    // 0.80:     0.4247,     0.2944,     0.3067
-    // 0.85:     0.4301,     0.3073,     0.2856
-    // 0.90:     0.4316,     0.3237,     0.2501
-    // 0.95:     0.4314,     0.3788,     0.1220
-    // In fact, for varying n and capacity, hmap is consistently faster than dict, except for very
-    // high or very low load factors. I get similar results if compiled with gcc.
 }
 
 void stress_dict(long n, long capacity, float load_factor, DictKeyHash hash)
 {
     char key[1024];
-    Dict dict = dict_create(capacity, load_factor, hash, sizeof(long), memcpy, free);
+    Dict dict = dict_create(capacity, load_factor, sizeof(long), hash, memcpy, free);
     for (long i = 0; i < n; ++i) dict_insert(&dict, number(key, i), &i);
     assert(dict.size == n);
     for (long i = 0; i < n; ++i) assert(dict_find(&dict, number(key, i)));
@@ -66,7 +88,7 @@ void stress_dict(long n, long capacity, float load_factor, DictKeyHash hash)
 void stress_hmap(long n, long capacity, float load_factor, HmapKeyHash hash)
 {
     char key[1024];
-    Hmap hmap = hmap_create(capacity, load_factor, hash, sizeof(long), memcpy, free);
+    Hmap hmap = hmap_create(capacity, load_factor, sizeof(long), hash, memcpy, free);
     for (long i = 0; i < n; ++i) hmap_insert(&hmap, number(key, i), &i);
     assert(hmap.size == n);
     for (long i = 0; i < n; ++i) assert(hmap_find(&hmap, number(key, i)));

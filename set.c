6fa04f53c98a58f6eabdd4a65dcc93a28007aa29
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-01-22 13:57:35 +0100

    add set utility functions

--- set.c
+++ set.c
@@ -6,6 +6,8 @@ static constexpr long mega_byte = 1 << 20;
 
 #define countof(A) ((long)(sizeof(A) / sizeof(*(A))))
 
+void set_print(const Set *set, const char *name);
+
 int main(void) {
     Arena arena = arena_create(mega_byte);
 
@@ -40,5 +42,25 @@ int main(void) {
     printf("set.find(ten) = %d\n", set_find(&set, "ten", 0));
     printf("clone.find(ten) = %d\n", set_find(&clone, "ten", 0));
 
+    Set set_or_clone = set_union(&set, &clone, nullptr);
+    set_print(&set_or_clone, "set_or_clone");
+    Set set_and_clone = set_intersection(&set, &clone, nullptr);
+    set_print(&set_and_clone, "set_and_clone");
+    Set set_minus_clone = set_difference(&set, &clone, nullptr);
+    set_print(&set_minus_clone, "set_minus_clone");
+    Set set_xor_clone = set_symmetric_difference(&set, &clone, nullptr);
+    set_print(&set_xor_clone, "set_xor_clone");
+
+    printf("set.is_subset(clone) = %d\n", set_is_subset(&set, &clone));
+    printf("set.is_superset(clone) = %d\n", set_is_superset(&set, &clone));
+
     arena_destroy(&arena);
+}
+
+void set_print(const Set *set, const char *name) {
+    printf("%s = {", name);
+    set_for_each(item, set) {
+        printf("%s, ", (char *)item->key.data);
+    }
+    printf("}\n");
 }
--- set.h
+++ set.h
@@ -31,3 +31,59 @@ static Set set_clone(const Set *self, Arena *arena) {
 static SetItem *set_items(const Set *self, Arena *arena) {
     return dict_items(self, arena);
 }
+
+static Set set_union(const Set *self, const Set *other, Arena *arena) {
+    Set set = set_clone(self, arena);
+    set_for_each(item, other) {
+        set_insert(&set, item->key.data, item->key.size);
+    }
+    return set;
+}
+
+static Set set_intersection(const Set *self, const Set *other, Arena *arena) {
+    Set set = set_create(arena ? arena : self->arena);
+    set_for_each(item, self) {
+        if (set_find(other, item->key.data, item->key.size)) {
+            set_insert(&set, item->key.data, item->key.size);
+        }
+    }
+    return set;
+}
+
+static Set set_difference(const Set *self, const Set *other, Arena *arena) {
+    Set set = set_create(arena ? arena : self->arena);
+    set_for_each(item, self) {
+        if (!set_find(other, item->key.data, item->key.size)) {
+            set_insert(&set, item->key.data, item->key.size);
+        }
+    }
+    return set;
+}
+
+static Set set_symmetric_difference(const Set *self, const Set *other, Arena *arena) {
+    Set set = set_create(arena ? arena : self->arena);
+    set_for_each(item, self) {
+        if (!set_find(other, item->key.data, item->key.size)) {
+            set_insert(&set, item->key.data, item->key.size);
+        }
+    }
+    set_for_each(item, other) {
+        if (!set_find(self, item->key.data, item->key.size)) {
+            set_insert(&set, item->key.data, item->key.size);
+        }
+    }
+    return set;
+}
+
+static bool set_is_subset(const Set *self, const Set *other) {
+    set_for_each(item, self) {
+        if (!set_find(other, item->key.data, item->key.size)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool set_is_superset(const Set *self, const Set *other) {
+    return set_is_subset(other, self);
+}

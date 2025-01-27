94c2fda128991f686beb6fbcb1141b7bf19553f9
hheinzer <heinrich.heinzer@gmail.com>
2025-01-27 17:56:09 +0000

    Deploying to gh-pages from @ hheinzer/cdsa@3ae9d9192af2ac53957d9c94b0fae68c6fe77fca 🚀

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['data_0',['data',['../structArena.html#a012ce4e3651af3acea4c90268e584713',1,'Arena::data()'],['../structListItem.html#a788104e1ae9d59a384497766e5203f20',1,'ListItem::data()']]]
+  ['data_0',['data',['../structArena.html#a012ce4e3651af3acea4c90268e584713',1,'Arena::data()'],['../structList.html#a0a12cc656feb2ac62b47dd6965371da6',1,'List::data()'],['../structListItem.html#a788104e1ae9d59a384497766e5203f20',1,'ListItem::data()']]]
 ];
--- structList.js
+++ structList.js
@@ -4,6 +4,7 @@ var structList =
     [ "size", "structList.html#a51ad903abfc5a3058a43a40588c5f6d4", null ],
     [ "compare", "structList.html#a297fc19bce2ca37d83eaf988edcecd71", null ],
     [ "copy", "structList.html#aa5951b57aec208d1f84e69381a6b9e76", null ],
+    [ "data", "structList.html#a0a12cc656feb2ac62b47dd6965371da6", null ],
     [ "length", "structList.html#a69fb993c1a2e12802de0c7d5a067d4cd", null ],
     [ "begin", "structList.html#ac8da70a91b5c8652254f129830f1ccf3", null ],
     [ "end", "structList.html#a6cd1ebc181a7dab2c89b8ee162106cb2", null ]
